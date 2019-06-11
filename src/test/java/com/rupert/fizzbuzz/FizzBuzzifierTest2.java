81cfe51ba8bd5363ea16f22c9961fd41d66f0c89
RupertWT <RupertWT@172.21.172.33>
2019-06-11 14:23:03 +0100

    ...

--- src/test/java/com/rupert/fizzbuzz/FizzBuzzifierTest2.java
+++ src/test/java/com/rupert/fizzbuzz/FizzBuzzifierTest2.java
@@ -2,9 +2,14 @@ package com.rupert.fizzbuzz;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 public class FizzBuzzifierTest2 {
+	
+	@Rule
+	public ExpectedException thrown= ExpectedException.none();
 	
 	FizzBuzzifier2 fb = new FizzBuzzifier2();
    
